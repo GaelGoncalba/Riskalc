e30e40f8dadedce3fba2bf29230091145640636d
Gael Gonçalves Alba <gaelgoncalvesalba@gmail.com>
2024-12-29 12:13:42 +0100

    Soluciona jacarmona364/Riskalc#10 corrige redundancias con Cliente

--- internal/models/operacion.go
+++ internal/models/operacion.go
@@ -6,33 +6,19 @@ import (
 
 type Operacion struct {
 	ID             string
-	Sueldo         float64
-	Deudas         float64
 	OperacionPagada bool
 }
 
-// Validaciones al crear una nueva operacion
-func NewOperacion(id string, sueldo, deudas float64, operacionPagada bool) (Operacion, error) {
-	// ID no vacio
+// Validaciones al crear una nueva operación
+func NewOperacion(id string, operacionPagada bool) (Operacion, error) {
+	// ID no vacío
 	if id == "" {
-		return Operacion{}, errors.New("el ID no puede estar vacio")
+		return Operacion{}, errors.New("el ID no puede estar vacío")
 	}
 
-	// Sueldo no negativo
-	if sueldo < 0 {
-		return Operacion{}, errors.New("el sueldo no puede ser negativo")
-	}
-
-	// Deudas no negativas
-	if deudas < 0 {
-		return Operacion{}, errors.New("las deudas no pueden ser negativas")
-	}
-
-	// Crear y devolver una nueva operacion
+	// Crear y devolver una nueva operación
 	return Operacion{
 		ID:             id,
-		Sueldo:         sueldo,
-		Deudas:         deudas,
 		OperacionPagada: operacionPagada,
 	}, nil
 }
