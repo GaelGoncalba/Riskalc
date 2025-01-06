48b7b6963e795ff3595ee17a3c22b05bc33888c1
Gael Gonçalves Alba <gaelgoncalvesalba@gmail.com>
2025-01-06 12:06:32 +0100

    Soluciona jacarmona364/Riskalc#12 añade el vector de operaciones a GestorRiesgos

--- internal/gestor_riesgos.go
+++ internal/gestor_riesgos.go
@@ -9,10 +9,12 @@ import (
 
 type GestorRiesgos struct {
 	Clientes            []models.Cliente
+	Operaciones	    []models.Operacion
 }
 
 func NuevoGestor(cliente models.Cliente) *GestorRiesgos {
 	return &GestorRiesgos{
 		Clientes:            []models.Cliente{},
+		Operaciones:         []models.Operacion{},
 	}
 }
