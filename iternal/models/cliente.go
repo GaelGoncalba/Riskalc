ff9482963c3d132086f2e769eb7a9cc07ff5940d
Gael Gonçalves Alba <gaelgoncalvesalba@gmail.com>
2025-01-10 08:48:37 +0100

    Soluciona jacarmona364/Riskalc#9 elimina el sueldo y las deudas de Cliente

--- iternal/models/cliente.go
+++ iternal/models/cliente.go
@@ -7,33 +7,19 @@ import (
 
 type Cliente struct {
 	DNI		 string
-	Sueldo           float64
-	Deudas           float64
 	OperacionHistorial []Operacion
 }
 
 // Validaciones al crear un nuevo cliente
-func NewCliente(dni string, sueldo, deudas float64) (Cliente, error) {
+func NewCliente(dni string) (Cliente, error) {
 	// ID no vacio
 	if dni == "" {
 		return Cliente{}, errors.New("el DNI no puede estar vacio")
 	}
 
-	// Sueldo no negativo
-	if sueldo < 0 {
-		return Cliente{}, errors.New("el sueldo no puede ser negativo")
-	}
-
-	// Deudas no negativas
-	if deudas < 0 {
-		return Cliente{}, errors.New("las deudas no pueden ser negativas")
-	}
-
 	// Crear y devolver un nuevo cliente
 	return Cliente{
 		DNI:              dni,
-		Sueldo:           sueldo,
-		Deudas:           deudas,
 		OperacionHistorial: []Operacion{}, // Inicializar el historial vacio
 	}, nil
 }
