8403eddccea1acd7f615a5951766331876cb2231
Gael Gonçalves Alba <gaelgoncalvesalba@gmail.com>
2025-01-10 08:58:07 +0100

    Soluciona jacarmona364/Riskalc#10 añade sueldo y deudas a Operacion

--- iternal/models/operacion.go
+++ iternal/models/operacion.go
@@ -5,20 +5,34 @@ import (
 )
 
 type Operacion struct {
-	ID             string
+	ID              string
+	Sueldo          float64
+	Deudas          float64
 	OperacionPagada bool
 }
 
 // Validaciones al crear una nueva operación
-func NewOperacion(id string, operacionPagada bool) (Operacion, error) {
+func NewOperacion(id string, sueldo float64, deudas float64, operacionPagada bool) (Operacion, error) {
 	// ID no vacío
 	if id == "" {
 		return Operacion{}, errors.New("el ID no puede estar vacío")
 	}
 
+	// sueldo no sea negativo
+	if sueldo < 0 {
+		return Operacion{}, errors.New("el sueldo no puede ser negativo")
+	}
+
+	// deudas no negativas
+	if deudas < 0 {
+		return Operacion{}, errors.New("las deudas no pueden ser negativas")
+	}
+
 	// Crear y devolver una nueva operación
 	return Operacion{
-		ID:             id,
+		ID:              id,
+		Sueldo:          sueldo,
+		Deudas:          deudas,
 		OperacionPagada: operacionPagada,
 	}, nil
 }
