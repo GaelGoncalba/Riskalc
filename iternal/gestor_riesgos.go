b7cb76f07526b1dbe2c352705fa3b038c94100ff
Gael Gonçalves Alba <gaelgoncalvesalba@gmail.com>
2025-01-12 11:10:41 +0100

    Soluciona jacarmona364/Riskalc#12 cambia la estructura de OperacionesHistoricas

--- iternal/gestor_riesgos.go
+++ iternal/gestor_riesgos.go
@@ -10,10 +10,10 @@ import (
 type GestorRiesgos struct {
 	Cliente               models.Cliente
 	NuevaOperacion        models.Operacion
-	OperacionesHistoricas map[string]models.Operacion
+	OperacionesHistoricas map[string]map[string]models.Operacion
 }
 
-func NuevoGestor(cliente models.Cliente, nuevaOperacion models.Operacion, operacionesHistoricas map[string]models.Operacion) *GestorRiesgos {
+func NuevoGestor(cliente models.Cliente, nuevaOperacion models.Operacion, operacionesHistoricas map[string]map[string]models.Operacion) *GestorRiesgos {
 	return &GestorRiesgos{
 		Cliente:            	cliente,
 		NuevaOperacion:		nuevaOperacion,
