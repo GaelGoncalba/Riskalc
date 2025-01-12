73b6a1fdf7ebcdd3b072517be61e94efe1ac8e44
Gael Gonçalves Alba <gaelgoncalvesalba@gmail.com>
2025-01-12 10:56:09 +0100

    Soluciona jacarmona364/Riskalc#10 tiene en cuenta nuevos objetos Sueldo y Deuda

--- iternal/models/operacion.go
+++ iternal/models/operacion.go
@@ -6,26 +6,16 @@ import (
 
 type Operacion struct {
 	ID              string
-	Sueldo          float64
-	Deudas          float64
+	Sueldo          Sueldo
+	Deudas          Deuda
 	OperacionPagada bool
 }
 
 // Validaciones al crear una nueva operación
-func NewOperacion(id string, sueldo float64, deudas float64, operacionPagada bool) (Operacion, error) {
+func NewOperacion(id string, sueldo Sueldo, deudas Deuda, operacionPagada bool) (Operacion, error) {
 	// ID no vacío
 	if id == "" {
 		return Operacion{}, errors.New("el ID no puede estar vacío")
-	}
-
-	// sueldo no sea negativo
-	if sueldo < 0 {
-		return Operacion{}, errors.New("el sueldo no puede ser negativo")
-	}
-
-	// deudas no negativas
-	if deudas < 0 {
-		return Operacion{}, errors.New("las deudas no pueden ser negativas")
 	}
 
 	// Crear y devolver una nueva operación
