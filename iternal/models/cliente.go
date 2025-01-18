452beea5987f3fc308983d589dc1cebd4c8414a6
Gael Gonçalves Alba <gaelgoncalvesalba@gmail.com>
2025-01-18 12:19:51 +0100

    Soluciona jacarmona364/Riskalc#9 convierto los campos a privados

--- iternal/models/cliente.go
+++ iternal/models/cliente.go
@@ -5,20 +5,20 @@ import (
 )
 
 type Cliente struct {
-	ID		   string
-	OperacionHistorial map[string]Operacion
+	id		   string
+	operacionHistorial map[string]Operacion
 }
 
 // Validaciones al crear un nuevo cliente
-func NewCliente(id string) (Cliente, error) {
+func NewCliente(id1 string) (Cliente, error) {
 	// ID no vacio
-	if id == "" {
+	if id1 == "" {
 		return Cliente{}, errors.New("el ID no puede estar vacio")
 	}
 
 	// Crear y devolver un nuevo cliente
 	return Cliente{
-		ID:                 id,
-		OperacionHistorial: make(map[string]Operacion), // Inicializar el historial como map vacio
+		id:                 id1,
+		operacionHistorial: make(map[string]Operacion), // Inicializar el historial como map vacio
 	}, nil
 }
