b0ebdc81ac9db7a1294b1e05902da5e90788200e
Gael Gonçalves Alba <gaelgoncalvesalba@gmail.com>
2025-01-18 12:25:23 +0100

    Soluciona jacarmona364/Riskalc#10 cambia el campo valor de Sueldo a privado

--- iternal/models/sueldo.go
+++ iternal/models/sueldo.go
@@ -5,18 +5,18 @@ import (
 )
 
 type Sueldo struct {
-	Valor float64
+	valor float64
 }
 
 // Validaciones al crear una nueva operación
-func NewSueldo(valor float64) (Sueldo, error) {
+func NewSueldo(valor1 float64) (Sueldo, error) {
 	// valor no negativo
-	if valor < 0 {
+	if valor1 < 0 {
 		return Sueldo{}, errors.New("el sueldo no puede ser negativo")
 	}
 
 	// Crear y devolver el sueldo
 	return Operacion{
-		Valor: valor,
+		valor: valor1,
 	}, nil
 }
